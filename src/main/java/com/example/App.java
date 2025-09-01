97fa3af92be61938e40155ae3afe4ccbaf8f8390
youssefdridi1977 <dridi1977@yahoo.fr>
2025-09-01 16:43:22 +0200

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,7 @@ package com.example;
 public class App {
     public static String getMessage() {
         return "Hello, Jenkins!";
-        
+        d
     }
 
     public static void main(String[] args) {
